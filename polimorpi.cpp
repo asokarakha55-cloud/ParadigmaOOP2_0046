e9d36626b1b3e17b549166235117f73d4f52696e
asokarakha55-cloud <asokarakha55@gmail.com>
2026-05-18 10:13:37 +0700

    Membuat class anak kedua

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -19,3 +19,11 @@ public:
         cout << "Pesan dari joko" << endl;
     }
 };
+class lia : public seseorang
+{
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia" << endl;
+    }
+};
